bae089fe6616317edfa738ad1b10c0cb75e0a6a5
caoyihong <1198059281@qq.com>
2017-05-08 22:25:44 +0800

    add

--- src/test/java/com/mkfree/sentinel/TestRedis.java
+++ src/test/java/com/mkfree/sentinel/TestRedis.java
@@ -18,9 +18,11 @@ public class TestRedis {
     @Before
     public void setup() {
         //连接redis服务器，192.168.0.100:6379
-        jedis = new Jedis("192.168.0.100", 6379);
+        jedis = new Jedis("192.168.140.134", 6381);
         //权限认证
-        jedis.auth("admin");
+        jedis.auth("myRedis");
+//        String result = jedis.ping();
+//        System.out.println(result);
     }
 
     /**
--- src/test/java/com/mkfree/sentinel/TestSentinel.java
+++ src/test/java/com/mkfree/sentinel/TestSentinel.java
@@ -5,6 +5,7 @@ import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.JedisSentinelPool;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -14,11 +15,11 @@ public class TestSentinel {
     private static final JedisSentinelPool pool;
     static {
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setMaxTotal(10);
+//        jedisPoolConfig.setMaxTotal(10);
         jedisPoolConfig.setMaxIdle(5);
         jedisPoolConfig.setMinIdle(5);
 
-        Set<String> sentinels = new HashSet<>(Arrays.asList(
+        Set<String> sentinels = new HashSet<String>(Arrays.asList(
                 "111.111.111.111:26379",
                 "111.111.111.112:26379",
                 "111.111.111.113:26379"
